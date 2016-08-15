ceac403076d176e20e436c1c5007aed8bca75211
Michał Pawłowski <unittestablecode@gmail.com>
2016-08-16 01:06:37 +0200

    Remove expilicit queue from the crawler.

--- aio_crawler/crawler.py
+++ aio_crawler/crawler.py
@@ -1,18 +1,20 @@
 """Crawling logic."""
 import logging
-import asyncio
+from asyncio import get_event_loop, wait, Task, TimeoutError
 import aiohttp
 
 from .reader import resources_from_tag_soup
 
 
-TIMEOUT = 10  # sec
-
 log = logging.getLogger(__name__)
 
 
-async def fetch_page(session, url):
-    """Grab a page and return a tuple of lists of links and resources."""
+async def fetch_page(session: aiohttp.ClientSession, url: str) -> tuple:
+    """
+    Grab a page and return a tuple of lists of links and resources.
+
+    :return: tuple of sets of links and all resources, including links
+    """
     async with session.get(url) as response:
         if not 200 <= response.status < 300:
             log.warning('Non-OK response (%s) received at %s', response.status, url)
@@ -29,52 +31,61 @@ async def fetch_page(session, url):
 class Crawler():
     """A crawler implemantation, mapping links and other resources of a single site."""
 
-    def __init__(self, address):
+    def __init__(self, address: str, timeout: float = 10.0):
         """Initialize the crawler."""
-        self.address = address
         self.url_to_results = {}
-        self.queue = asyncio.Queue()
+        self.address = address
+        self.timeout = timeout
+        self.loop = get_event_loop()
 
-    async def fetching_worker(self, session):
-        """Repeatedly run the fetching coroutine and update visited URLs."""
-        while True:
-            address = await self.queue.get()
-            self.url_to_results[address] = None
-            log.debug('Visiting %s', address)
+    async def fetching_worker(self, address: str, session: aiohttp.ClientSession):
+        """
+        Grab the page, discover links and run coroutines to fetch them.
 
+        Coroutines for grabbing resources are opened eagerly. They don't start
+        all at once anyway because the `session` has its internal pool.
+
+        At the end of the coroutine we check if the current coroutine is the last
+        remaining one on the event loop. If so, that means no other links are left
+        to consume.
+        """
+        log.info('Visiting %s', address)
+
+        try:
+            with aiohttp.Timeout(self.timeout):
+                links, resources = await fetch_page(session, address)
+        except TimeoutError as e:
+            log.warning('Timed out, requeueing the resource: %s', address)
+            self.loop.create_task(self.fetching_worker(address, session))
+
+        self.url_to_results[address] = (links, resources)
+
+        for url in links:
+            # Eagerly creating coroutines for unseen links.
+            if url not in self.url_to_results:
+                self.url_to_results[address] = None  # Marking links as seen.
+                self.loop.create_task(self.fetching_worker(url, session))
+
+        if Task.all_tasks() == {Task.current_task()}:
+            # No other resources to grab.
+            self.loop.stop()
+
+    def crawl(self, concurrency: int = 20) -> dict:
+        """
+        Crawl the entire website.
+
+        :return: A dict with crawling results.
+        """
+        connector = aiohttp.TCPConnector(limit=concurrency)
+        with aiohttp.ClientSession(loop=self.loop, connector=connector) as session:
+            self.loop.create_task(self.fetching_worker(self.address, session))
             try:
-                with aiohttp.Timeout(TIMEOUT):
-                    links, resources = await fetch_page(session, address)
-            except asyncio.TimeoutError as e:
-                log.warning('Timed out, requeueing the resource: %s', address)
-                self.queue.put_nowait(address)
-                continue
-
-            self.url_to_results[address] = (links, resources)
-
-            for url in links:
-                if url not in self.url_to_results:
-                    self.queue.put_nowait(url)
-                self.url_to_results.setdefault(None)
-            self.queue.task_done()
-
-    def crawl(self, concurrency=20):
-        """Crawl the entire website."""
-        loop = asyncio.get_event_loop()
-        self.queue.put_nowait(self.address)
-
-        with aiohttp.ClientSession(loop=loop) as session:
-            tasks = [loop.create_task(self.fetching_worker(session))
-                     for _ in range(concurrency)]
-
-            try:
-                loop.run_until_complete(self.queue.join())
+                self.loop.run_forever()
             except KeyboardInterrupt:
                 log.warning('Keyboard interrupt. Will return partial results.')
-                raise
-            finally:
+                tasks = Task.all_tasks()
                 for task in tasks:
                     task.cancel()
-                loop.run_until_complete(asyncio.wait(tasks))
-
+                self.loop.run_until_complete(wait(tasks))
+            finally:
                 return self.url_to_results
